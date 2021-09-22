5e21744a32ea4e400938c21de76ca3dab23390e3
Samuel Venable <samuelvenable@hotmail.com>
2021-09-22 06:47:41 -0400

    Add files via upload

--- build64.sh
+++ build64.sh
@@ -3,13 +3,13 @@ cd "${0%/*}"
 cd "src"
 
 if [ $(uname) = "Darwin" ]; then
-  clang++ widenarrow.cpp rgbtorgba.cpp pngformat.cpp bmpformat.cpp libpixeldata.cpp /opt/local/lib/libz.a /opt/local/lib/libpng.a -I../include/ -o ../libPixelData.dylib -std=c++17 -shared -I/opt/local/include -m64 -fPIC
+  clang++ widenarrow.cpp rgbtorgba.cpp pngformat.cpp bmpformat.cpp libpixeldata.cpp /opt/local/lib/libpng.a /opt/local/lib/libz.a -I../include/ -o ../libPixelData.dylib -std=c++17 -shared -I/opt/local/include -m64 -fPIC
 elif [ $(uname) = "Linux" ]; then
-  g++ widenarrow.cpp rgbtorgba.cpp pngformat.cpp bmpformat.cpp libpixeldata.cpp -I../include/ -o ../libPixelData.so -std=c++17 -shared -static-libgcc -static-libstdc++ -lz -lpng -m64 -fPIC
+  g++ widenarrow.cpp rgbtorgba.cpp pngformat.cpp bmpformat.cpp libpixeldata.cpp -I../include/ -o ../libPixelData.so -std=c++17 -shared -static-libgcc -static-libstdc++ -lpng -lz -m64 -fPIC
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ widenarrow.cpp rgbtorgba.cpp pngformat.cpp bmpformat.cpp libpixeldata.cpp -I../include/ -o ../libPixelData.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -lz -lpng -m64 -fPIC
+  clang++ widenarrow.cpp rgbtorgba.cpp pngformat.cpp bmpformat.cpp libpixeldata.cpp -I../include/ -o ../libPixelData.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -lpng -lz -m64 -fPIC
 elif [ $(uname) = "DragonFly" ]; then
-  g++ widenarrow.cpp rgbtorgba.cpp pngformat.cpp bmpformat.cpp libpixeldata.cpp -I../include/ -o ../libPixelData.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -static-libgcc -static-libstdc++ -lz -lpng -m64 -fPIC
+  g++ widenarrow.cpp rgbtorgba.cpp pngformat.cpp bmpformat.cpp libpixeldata.cpp -I../include/ -o ../libPixelData.so -std=c++17 -shared -I/usr/local/include -L/usr/local/lib -static-libgcc -static-libstdc++ -lpng -lz -m64 -fPIC
 else
-  g++ widenarrow.cpp rgbtorgba.cpp pngformat.cpp bmpformat.cpp libpixeldata.cpp -I../include/ -o ../libPixelData.dll -std=c++17 -shared -static-libgcc -static-libstdc++ -static -lz -lpng -m64
+  g++ widenarrow.cpp rgbtorgba.cpp pngformat.cpp bmpformat.cpp libpixeldata.cpp -I../include/ -o ../libPixelData.dll -std=c++17 -shared -static-libgcc -static-libstdc++ -static -lpng -lz -m64
 fi
