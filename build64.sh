3d8ebefbd006e8ee7295f4419ce103465fec7199
Samuel Venable <samuelvenable@hotmail.com>
2021-10-20 00:17:13 -0400

    Update build64.sh

--- build64.sh
+++ build64.sh
@@ -3,7 +3,7 @@ cd "${0%/*}"
 cd "src"
 
 if [ $(uname) = "Darwin" ]; then
-  clang++ widenarrow.cpp rgbtorgba.cpp pngformat.cpp lodepng.cpp bmpformat.cpp libpixeldata.cpp /opt/local/lib/libpng.a /opt/local/lib/libz.a -I../include/ -o ../libPixelData.dylib -std=c++17 -shared -I/opt/local/include -m64 -fPIC
+  clang++ widenarrow.cpp rgbtorgba.cpp pngformat.cpp lodepng.cpp bmpformat.cpp libpixeldata.cpp /opt/local/lib/libpng.a /opt/local/lib/libz.a -I../include/ -o ../libPixelData.dylib -std=c++17 -shared -I/opt/local/include -arch arm64 -arch x86_64 -fPIC
 elif [ $(uname) = "Linux" ]; then
   g++ widenarrow.cpp rgbtorgba.cpp pngformat.cpp lodepng.cpp bmpformat.cpp libpixeldata.cpp -I../include/ -o ../libPixelData.so -std=c++17 -shared -static-libgcc -static-libstdc++ -lpng -lz -m64 -fPIC
 elif [ $(uname) = "FreeBSD" ]; then
